688b1ef04b5b4e16fa366dd6765bb91f7c9a0264
Wei Chen <81772788+wchemz@users.noreply.github.com>
2023-08-30 12:50:09 -0500

    Update ChimeMeetingCC.user.js

--- scripts/ChimeMeetingCC.user.js
+++ scripts/ChimeMeetingCC.user.js
@@ -8,7 +8,7 @@
 // @grant        GM_xmlhttpRequest
 // @updateURL    https://github.com/wchemz/ChimeChiefMate/raw/main/scripts/ChimeMeetingCC.user.js
 // @downloadURL  https://github.com/wchemz/ChimeChiefMate/raw/main/scripts/ChimeMeetingCC.user.js
-// @connect      https://6upnfpnrt8.execute-api.us-east-1.amazonaws.com
+// @connect      6upnfpnrt8.execute-api.us-east-1.amazonaws.com
 // ==/UserScript==
 
 (function () {
